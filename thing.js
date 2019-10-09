9c4a8c7caf6a6a0c7af2066ab836bb22a0e6f676
Ryan Hanlon <rjhanlon@quinnipiac.edu>
2019-10-09 13:50:51 -0400

    Added additional support for calculating Ascii sum and Hex representation

--- thing.js
+++ thing.js
@@ -1,7 +1,30 @@
 function update() {
 	var str = document.getElementById("text").value;
+	reverse(str);
+	asciiSum(str);
+	hex(str);
+}
+function reverse(input){
 	var outstr = "";
-	for (var i = str.length - 1; i >= 0; i--)
-		outstr += str[i];
+	for (var i = input.length - 1; i >= 0; i--)
+		outstr += input[i];
 	document.getElementById("textout").value = outstr;
+}
+
+
+function asciiSum(input){
+	var sum = 0;
+	for (let i = 0; i < input.length; i++) 
+		sum += input.charCodeAt(i);
+	document.getElementById("textoutAss").value = sum;
+}
+
+function hex(input){
+	var arr = [];
+	var outstr = "";
+	for (let i = 0, l = input.length; i < l; i++) {
+		var hex = Number(input.charCodeAt(i)).toString(16);
+		arr.push(hex);
+	}
+	document.getElementById("textoutHex").value = arr.join('');
 }
