2b73564193f4f725613f05ef27d1ffca4682fe3f
DizzleDuff <31937154+DizzleDuff@users.noreply.github.com>
2019-10-10 16:50:03 -0400

    added palindrome input

--- thing.js
+++ thing.js
@@ -3,8 +3,10 @@ function update() {
 	reverse(str);
 	asciiSum(str);
 	hex(str);
+	palindrome(str);
 }
-function reverse(input){
+
+function reverse(input) {
 	var outstr = "";
 	for (var i = input.length - 1; i >= 0; i--)
 		outstr += input[i];
@@ -12,14 +14,14 @@ function reverse(input){
 }
 
 
-function asciiSum(input){
+function asciiSum(input) {
 	var sum = 0;
-	for (let i = 0; i < input.length; i++) 
+	for (let i = 0; i < input.length; i++)
 		sum += input.charCodeAt(i);
 	document.getElementById("textoutAss").value = sum;
 }
 
-function hex(input){
+function hex(input) {
 	var arr = [];
 	var outstr = "";
 	for (let i = 0, l = input.length; i < l; i++) {
@@ -27,4 +29,18 @@ function hex(input){
 		arr.push(hex);
 	}
 	document.getElementById("textoutHex").value = arr.join('');
+}
+
+function palindrome(str) {
+	var re = /[^A-Za-z0-9]/g;
+	str = str.toLowerCase().replace(re, '');
+	var len = str.length;
+	for (var i = 0; i < len / 2; i++) {
+		if (str[i] !== str[len - 1 - i]) {
+			return false;
+		}
+	}
+	document.getElementById("textoutPalindrome").value = str;
+
+	return true;
 }
